c4a5773170c838aabaab6caad79b608a5d2158fe
altelv <163553710+altelv@users.noreply.github.com>
2025-08-30 11:05:18 +0300

    Update index.ts

--- src/game/dice/index.ts
+++ src/game/dice/index.ts
@@ -1,3 +1,21 @@
-export * from "./D20RollLauncher";
-export * from "./D20Overlay";
-export * from "./D20RollService";
+// src/game/dice/index.ts
+// Баррель-экспорт для модуля кубика D20.
+
+export { D20Overlay } from "./D20Overlay";
+export { D20RollLauncher } from "./D20RollLauncher";
+export { resolveRoll } from "./D20RollService";
+
+// Пробрасываем типы наружу (удобно для App и UI)
+export type { RollRequest, RollResolution, CharacterState } from "../state/types";
+
+// Стандартные пути к ассетам (если хочешь импортировать “готовым пакетом”)
+export const D20_ASSET_PATHS = {
+  spinFrames: [
+    "/assets/d20/spin_1.png",
+    "/assets/d20/spin_2.png",
+    "/assets/d20/spin_3.png",
+    "/assets/d20/spin_4.png",
+  ],
+  resultDefault: "/assets/d20/result_default.png",
+  resultSpecial: "/assets/d20/result_special.png",
+};
