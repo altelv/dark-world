7ed4ae363777aca51886381aaae21bae575f7686
altelv <163553710+altelv@users.noreply.github.com>
2025-08-30 11:53:32 +0300

    Update index.ts

--- src/game/dice/index.ts
+++ src/game/dice/index.ts
@@ -5,7 +5,7 @@ export { D20Overlay } from "./D20Overlay";
 export { D20RollLauncher } from "./D20RollLauncher";
 export { resolveRoll } from "./D20RollService";
 
-// Пробрасываем типы наружу (удобно для App и UI)
+// Пробрасываем типы наружу
 export type { RollRequest, RollResolution, CharacterState } from "../state/types";
 
 // Стандартные пути к ассетам (если хочешь импортировать “готовым пакетом”)
@@ -14,8 +14,8 @@ export const D20_ASSET_PATHS = {
     "/assets/d20/spin_1.png",
     "/assets/d20/spin_2.png",
     "/assets/d20/spin_3.png",
-    "/assets/d20/spin_4.png",
+    "/assets/d20/spin_4.png"
   ],
   resultDefault: "/assets/d20/result_default.png",
-  resultSpecial: "/assets/d20/result_special.png",
+  resultSpecial: "/assets/d20/result_special.png"
 };
