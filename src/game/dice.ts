73cb056c4bf9f4e53832da492b3879c4ba64dfbb
altelv <163553710+altelv@users.noreply.github.com>
2025-08-30 14:00:10 +0300

    Update dice.ts

--- src/game/dice.ts
+++ src/game/dice.ts
@@ -1,14 +1,7 @@
-// src/game/dice.ts
-// ШИМ для надёжного импорта "./game/dice": реэкспортируем всё из папки.
-
 export { D20Overlay } from "./dice/D20Overlay";
 export { resolveRoll } from "./dice/D20RollService";
-
-// И ДВУМЯ способами отдаём лаунчер:
-export { D20RollLauncher } from "./dice/D20RollLauncher";     // именованный
-export { default as default } from "./dice/D20RollLauncher";  // дефолт
-
-// На всякий случай пробрасываем все именованные экспорты:
+export { D20RollLauncher } from "./dice/D20RollLauncher";
+export { default as default } from "./dice/D20RollLauncher";
 export * from "./dice/D20Overlay";
 export * from "./dice/D20RollService";
 export * from "./dice/D20RollLauncher";
