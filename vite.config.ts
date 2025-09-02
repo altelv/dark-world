74a181f7188cb39be5669a278f0eece6a7b876b9
altelv <163553710+altelv@users.noreply.github.com>
2025-09-02 11:09:45 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@engine": fileURLToPath(new URL("./src/engine", import.meta.url)),
+      "@types": fileURLToPath(new URL("./src/types", import.meta.url)),
+      "@store": fileURLToPath(new URL("./src/store", import.meta.url)),
+      "@components": fileURLToPath(new URL("./src/components", import.meta.url)),
+      "@orchestrator": fileURLToPath(new URL("./src/orchestrator", import.meta.url)),
+      "@data": fileURLToPath(new URL("./src/game/data", import.meta.url)),
+      "@utils": fileURLToPath(new URL("./src/utils", import.meta.url))
+    }
+  },
   server: { port: 5173 },
   preview: { port: 5173 },
   build: { sourcemap: true }
